817a476bc2cd9bea9c9c68ba40cccd16a08d9e24
brasileiroapps <100478178+brasileiroapps@users.noreply.github.com>
2022-07-19 23:13:44 -0300

    change host

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL:"http://localhost:3333",
-    // baseURL:"http://45.178.182.24:3333/",
+    // baseURL:"http://localhost:3333",
+    baseURL:"http://45.178.182.24:3333/",
 })
 
 export default Api;
