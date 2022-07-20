f3f8b883d81812e94ee2f8d74a468769b85253ff
brasileiroapps <100478178+brasileiroapps@users.noreply.github.com>
2022-07-20 01:58:00 -0300

    add certificate

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
     // baseURL:"http://localhost:3333",
-    baseURL:"http://45.178.182.24:3333/",
+    baseURL:"https://45.178.182.24:3333/",
 })
 
 export default Api;
